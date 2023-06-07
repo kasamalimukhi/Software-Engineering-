1090a34d6630e9c9f19b3d542cbf2f2b85602333
kasamalimukhi <kasamalimukhi003@gmail.com>
2023-06-07 14:09:02 +0530

    module 4.0 assignment

---
+++ C++/module 4.0 -C++/4.1 basic/basic.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+// c++ header file
+using namespace std;
+int main()
+{
+	
+	cout<<"Hello";
+	cout<<"\n"<<"Hello";
+	cout<<endl<<"Hello";
+	
+	return 0;
+}
---
+++ C++/module 4.0 -C++/4.2 assgment/11. Calcu Area using fun overlo.cpp
@@ -0,0 +1,67 @@
+/*Write a program to calculate the area of circle, rectangle and triangle using
+Function Overloading
+? Rectangle: Area * breadth
+? Triangle: ? *Area* breadth
+? Circle: Pi * Area *Area*/
+#include<iostream>
+using namespace std; 
+
+class base
+{
+public:
+	int cho,b,h,r,l,w;
+	float area;
+
+void getdata()
+{
+cout<<"*****************Menu***************** \n";
+cout<<"1. Triangle\n";
+cout<<"2. Cricle\n";
+cout<<"3. Rectangle\n";
+
+cout<<"Select your choice:";
+cin>>cho;
+	
+}
+};
+class letdata : public base
+{
+public:
+void sdata()
+{
+	if (cho==1)
+	{
+		cout<<"Enter Base & Height:";
+		cin>>b>>h;
+		area=(b*h)/2;
+		cout<<"Area of Triangle:"<<area;
+	}
+	else if (cho==2)
+	{
+		cout<<"Enter radius of circle:";
+		cin>>r;
+		area=3.142*r*r;
+		cout<<"Area of circle:"<<area;
+	}
+	else if (cho==3)
+	{
+		cout<<"Enter Length & Width:";
+		cin>>l>>w;
+		area=l*w;
+	    cout<<"Area of Rectangle:"<<area;
+	}
+	else
+	{
+			cout<<"invalid output";
+	}	
+}
+};
+
+int main()
+{
+letdata d;
+d.getdata();
+d.sdata();
+
+return 0;
+}
